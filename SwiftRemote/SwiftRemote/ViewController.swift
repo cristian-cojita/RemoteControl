6ad1206922ba3e85083035fdec6f4675ccfa2883
Cristian Cojita <cristian@cojita.com>
2014-09-16 19:24:53 +0300

    Add Alamofire

--- SwiftRemote/SwiftRemote/ViewController.swift
+++ SwiftRemote/SwiftRemote/ViewController.swift
@@ -26,6 +26,41 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func GateTouchDown(sender: UIButton) {
+        println("Gate touch down")
+        Alamofire.request(.POST, "https://api.spark.io/v1/devices/53ff70065075535126451187/led", parameters: ["access_token":"c90aee533c514ae2b0efd5ecd1f765ae927c03a8", "params":"l1,HIGH"])
+            .responseJSON {(request, response, JSON, error) in
+                println(JSON)
+        }
+        
+    }
+    
+    @IBAction func GateTouchUpInside(sender: UIButton) {
+        println("Gate up inside")
+        Alamofire.request(.POST, "https://api.spark.io/v1/devices/53ff70065075535126451187/led", parameters: ["access_token":"c90aee533c514ae2b0efd5ecd1f765ae927c03a8", "params":"l1,LOW"])
+            .responseJSON {(request, response, JSON, error) in
+                println(JSON)
+        }
+    }
+    
+    @IBAction func DoorTouchDown(sender: UIButton) {
+        println("Door touch down")
+        Alamofire.request(.POST, "https://api.spark.io/v1/devices/53ff70065075535126451187/led", parameters: ["access_token":"c90aee533c514ae2b0efd5ecd1f765ae927c03a8", "params":"l2,HIGH"])
+            .responseJSON {(request, response, JSON, error) in
+                println(JSON)
+        }
+        
+    }
+    
+    @IBAction func DoorTouchUpInside(sender: UIButton) {
+        println("Door up inside")
+        Alamofire.request(.POST, "https://api.spark.io/v1/devices/53ff70065075535126451187/led", parameters: ["access_token":"c90aee533c514ae2b0efd5ecd1f765ae927c03a8", "params":"l2,LOW"])
+            .responseJSON {(request, response, JSON, error) in
+                println(JSON)
+        }
+    }
+
 
 
 }
